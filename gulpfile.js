93f5590338954c502e12d82a4087d797bbfa529c
Andre Nanninga <andre.nanninga@buyways.nl>
2015-03-13 10:44:16 +0100

    added support for regpack to save ~200 bytes

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,14 @@
 'use strict';
 
-var gulp     = require('gulp');
-var connect  = require('gulp-connect');
-var uglify   = require('gulp-uglify');
-var bytediff = require('gulp-bytediff');
-var plumber  = require('gulp-plumber');
+var gulp        = require('gulp');
+var connect     = require('gulp-connect');
+var uglify      = require('gulp-uglify');
+var plumber     = require('gulp-plumber');
+var run         = require('gulp-run');
+var rename      = require('gulp-rename');
+var size        = require('gulp-size');
+
+var runSequence = require('run-sequence');
 
 gulp.task('webserver', function() {
   connect.server({
@@ -13,17 +17,38 @@ gulp.task('webserver', function() {
 });
 
 gulp.task('uglify', function() {
-  gulp.src('src/1k.js')
+  return gulp.src('src/1k.js')
     .pipe(plumber())
-    .pipe(bytediff.start())
     .pipe(uglify())
-    .pipe(bytediff.stop())
+    .pipe(rename('1k.uglify.js'))
     .pipe(gulp.dest('dist'))
     .pipe(connect.reload());
 });
 
-gulp.task('watch', function() {
-  gulp.watch('src/*.js', ['uglify']);
+gulp.task('regpack', function() {
+  return gulp.src('.')
+    .pipe(plumber())
+    .pipe(run('node lib/regpack.js dist/1k.uglify.js', { verbosity: 0 }))
+    .pipe(rename('1k.regpack.js'))
+    .pipe(gulp.dest('dist'))
+    .pipe(connect.reload());
 });
 
-gulp.task('default', ['uglify', 'webserver', 'watch']);
+gulp.task('size', function() {
+  return gulp.src('dist/*.js')
+    .pipe(size({ showFiles: true }));
+});
+
+gulp.task('watch', function() {
+  gulp.watch('src/*.js', ['build']);
+});
+
+gulp.task('build', function() {
+  runSequence(
+    'uglify',
+    'regpack',
+    'size'
+  );
+});
+
+gulp.task('default', ['build', 'webserver', 'watch']);
